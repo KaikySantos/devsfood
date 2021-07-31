c5575701c722bbb14c293196f8c240333e10d24e
KaikySantos <info.kaiky@gmail.com>
2021-07-31 14:44:52 -0300

    menu lateral criado

---
+++ frontend/src/components/MenuItem/index.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import { useHistory, useLocation } from 'react-router';
+import { LinkArea, LinkIcon } from './styled';
+
+const MenuItem = ({ icon, link }) => {
+    const history = useHistory();
+    const location = useLocation();
+
+    let isActive = location.pathname === link;
+
+    const handleLinkClick = (e) => {
+        e.preventDefault();
+        history.push(link);
+    }
+
+    return (
+        <LinkArea active={isActive} href={link} onClick={handleLinkClick}>
+            <LinkIcon src={icon} />
+        </LinkArea>
+    );
+}
+
+export default MenuItem;
