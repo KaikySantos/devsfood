63a1c5e40c4dd7ac341532ec0b2082bfa1c5420c
KaikySantos <info.kaiky@gmail.com>
2021-08-06 20:53:12 -0300

    carrinho finalizado

--- frontend/src/components/Cart/index.js
+++ frontend/src/components/Cart/index.js
@@ -1,21 +1,73 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+
 import {
     CartArea,
     CartHeader,
     CartBody,
     CartIcon,
-    CartText
+    CartText,
+    CartDown,
+    ProductsArea,
+    ProductItem,
+    ProductPhoto,
+    ProductInfoArea,
+    ProductQuantityArea,
+    ProductName,
+    ProductPrice,
+    ProductQtIcon,
+    ProductQtText
 } from './styled';
 
 const Cart = () => {
+    const dispatch = useDispatch();
+    const products = useSelector(state => state.cart.products);
+
+    const [show, setShow] = useState(false);
+
+    const handleCartClick = () => {
+        setShow(!show);
+    }
+
+    const handleProductChange = (key, type) => {
+        dispatch({
+            type: 'CHANGE_PRODUCT',
+            payload: {key,type}
+        });
+    }
+
     return (
         <CartArea>
-            <CartHeader>
+            <CartHeader onClick={handleCartClick}>
                 <CartIcon src='/assets/cart.png'></CartIcon>
-                <CartText>Meu Carrinho (X)</CartText>
+                <CartText>Meu Carrinho ({products.length})</CartText>
+                <CartDown show={show} src="/assets/down.png" alt="" />
             </CartHeader>
-            <CartBody>
-
+            <CartBody show={show}>
+                <ProductsArea>
+                    {products.map((item, index) => (
+                        <ProductItem key={index}>
+                            <ProductPhoto src={item.image} alt="" />
+                            <ProductInfoArea>
+                                <ProductName>{item.name}</ProductName>
+                                <ProductPrice>R$ {(item.price).toFixed(2)}</ProductPrice>
+                            </ProductInfoArea>
+                            <ProductQuantityArea>
+                                <ProductQtIcon 
+                                    src="/assets/minus.png" 
+                                    alt="" 
+                                    onClick={() => handleProductChange(index, '-')}
+                                />
+                                <ProductQtText>{item.qt}</ProductQtText>
+                                <ProductQtIcon 
+                                    src="/assets/plus.png" 
+                                    alt="" 
+                                    onClick={() => handleProductChange(index, '+')}
+                                />
+                            </ProductQuantityArea>
+                        </ProductItem>
+                    ))}
+                </ProductsArea>
             </CartBody>
         </CartArea>
     );
--- frontend/src/components/Cart/styled.js
+++ frontend/src/components/Cart/styled.js
@@ -24,10 +24,66 @@ export const CartIcon = styled.img`
 `;
 
 export const CartText = styled.div`
+    flex: 1;
     color: #FFF;
     font-size: 17px;
 `;
 
 export const CartBody = styled.div`
+    color: #FFF;
+    transition: height 5s;
+    height: ${props=>props.show ? 'auto' : '0px'};
+`;
 
+export const CartDown = styled.img`
+    width: 23px;
+    height: auto;
+    margin: 0px 10px;
+    transition: all ease 0.65s;
+    transform: ${props=>props.show ? 'rotate(0deg)' : 'rotate(180deg)'};
+`;
+
+export const ProductsArea = styled.div`
+`;
+
+export const ProductItem = styled.div`
+    display: flex;
+    margin: 10px;
+`;
+
+export const ProductPhoto = styled.img`
+    width: 64px;
+    height: auto;
+    border-radius: 10px;
+`;
+
+export const ProductInfoArea = styled.div`
+    flex: 1;
+    margin-left: 10px;
+`;
+
+export const ProductQuantityArea = styled.div`
+    display: flex;
+    align-items: center;
+`;
+
+export const ProductName = styled.div`
+    font-size: 15px;
+    font-weight: bold;
+`;
+
+export const ProductPrice = styled.div`
+    font-size: 13px;
+`;
+
+export const ProductQtIcon = styled.img`
+    width: 13px;
+    height: auto;
+    cursor: pointer;
+`;
+
+export const ProductQtText = styled.div`
+    font-size: 13px;
+    font-weight: bold;
+    margin: 0px 5px;
 `;
